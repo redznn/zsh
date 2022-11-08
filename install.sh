3e22e8998cdfbe03418114f5d8e58d4ee3667b8f
redznn <103375250+redznn@users.noreply.github.com>
2022-11-08 18:25:16 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+# Executar comandos a seguir para atualizar os pacotes
+sudo apt update -y
+
+# Instalar pacotes a seguir
+sudo apt install git curl build-essential dkms perl wget -y
+sudo apt install gcc make default-libmysqlclient-dev libssl-dev -y
+sudo apt install -y zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm \
+  libncurses5-dev libncursesw5-dev \
+  xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
+  
+# Instalar e configurar ZSH
+sudo apt install zsh -y
+chsh -s /bin/zsh
+zsh
+
+# Instalar Oh-my-zsh! -> https://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar Racotechnic
+curl -L https://github.com/elboletaire/zsh-theme-racotecnic/raw/master/racotecnic.zsh-theme > ~/.oh-my-zsh/themes/racotecnic.zsh-theme
+
+# Mudar ~/.zshrc -> ZSH_THEME="racotecnic"
+
+# Instalar Zsh Autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Instalar Zsh Syntax Highlighting
+# https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Mudar plugins
+# plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
