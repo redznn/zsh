1def21e98dcd66489be0e0a1564f142ed758ae06
redznn <h908659@gmail.com>
2023-07-12 17:05:18 -0300

    install.sh: Code improvements
    Signed-off-by: redznn <h908659@gmail.com>

--- install.sh
+++ install.sh
@@ -2,30 +2,28 @@
 
 # Instalar e configurar ZSH
 sudo pacman -S zsh --noconfirm
-sleep 5
 chsh -s /bin/zsh
 
 # Removendo configurações antigas do oh my zsh se existir
-rm -rf ~/.oh-my-zsh
-rm -rf ~/.zshrc
+if [ -d "$HOME/.oh-my-zsh" ]; then
+  rm -rf "$HOME/.oh-my-zsh"
+fi
+
+if [ -f "$HOME/.zshrc" ]; then
+  rm "$HOME/.zshrc"
+fi
 
 # Instalar Oh-my-zsh! -> https://ohmyz.sh/
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Instalar biratime
+# Baixar biratime
 curl -fsSL -o ~/.oh-my-zsh/custom/themes/biratime.zsh-theme https://raw.github.com/vemonet/biratime/main/biratime.zsh-theme
 
 # Mudar tema do zsh
 sed -i 's/ZSH_THEME=".*/ZSH_THEME="biratime"/g' ~/.zshrc
 
-# Instalar Zsh Autosuggestions
-# https://github.com/zsh-users/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Instalar Zsh Syntax Highlighting
-# https://github.com/zsh-users/zsh-syntax-highlighting
+# Clonar Zsh Syntax Highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Mudar plugins
-sed -i 's/^plugins=(/plugins=(virtualenv git zsh-autosuggestions zsh-syntax-highlighting /g' ~/.zshrc
-
+# Ativar plugins
+sed -i 's/^plugins=(/plugins=(virtualenv git zsh-syntax-highlighting /g' ~/.zshrc
